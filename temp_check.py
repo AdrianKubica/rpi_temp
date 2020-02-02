81f57d73fbc0ca40b3026be3fb9ce3ae555e496a
Adrian <adrian.kubica.ak@gmail.com>
2020-02-02 13:29:32 +0000

    Update with time counter

--- temp_check.py
+++ temp_check.py
@@ -5,6 +5,11 @@ def measure_temp():
     temp = os.popen('vcgencmd measure_temp').readline()
     return temp.replace("temp=", "")
 
-while True:
-    print(measure_temp())
-    time.sleep(1)
+def main():
+    start = time.perf_counter()
+    while True:
+        print(f'{measure_temp()} after {round(time.perf_counter() - start, 2)} seconds')
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
